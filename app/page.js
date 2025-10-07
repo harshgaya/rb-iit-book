509f33032dfe4ddf1ab45c082c6b59e453944733
Yout701 <harshkumarworld@gmail.com>
2025-10-07 16:04:21 +0530

    a

--- app/page.js
+++ app/page.js
@@ -2,8 +2,6 @@ import ImageSlider from "@/components/home/image-slider";
 import TestimonialsPage from "@/components/home/testimonials";
 import WhyRBBooksPage from "@/components/home/why-rb-books";
 import ProductsHome from "@/components/home/products";
-
-import Image from "next/image";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
