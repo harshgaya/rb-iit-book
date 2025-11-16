7d3422cb49c134aef245bc703c11b3ea1f62283b
Yout701 <harshkumarworld@gmail.com>
2025-11-16 09:07:03 +0530

    a

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,33 +1,97 @@
+// import { setUserSession } from "@/lib/utils/action";
+// import {
+//   API_URL,
+//   GOOGLE_CLIENT_ID,
+//   GOOGLE_CLIENT_SECRET,
+// } from "@/lib/utils/constants";
+
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// export const authOptions = {
+//   secret: "fe51dd4501b392b42c5ec446c9287fac1b3aec6e86e4c4fb429de146b4a18bf8",
+//   providers: [
+//     GoogleProvider({
+//       clientId: GOOGLE_CLIENT_ID,
+//       clientSecret: GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   session: { strategy: "jwt" },
+//   pages: { signIn: "/sign-in" },
+//   basePath: "/api/auth",
+//   debug: true,
+//   trustHost: true,
+//   // 👇 Hardcode your live domain so NextAuth knows where to redirect
+//   //site: "https://book.softplix.com",
+
+//   callbacks: {
+//     async signIn({ user }) {
+//       try {
+//         // Call your API to store/fetch DB user
+//         const res = await fetch(`${API_URL}/sign-in`, {
+//           method: "POST",
+//           headers: { "Content-Type": "application/json" },
+//           body: JSON.stringify({
+//             id: user.id,
+//             name: user.name,
+//             email: user.email,
+//             image: user.image,
+//           }),
+//         });
+
+//         const data = await res.json();
+//         if (data.status === 200 && data.data[0]?._id) {
+//           user.user_id = data.data[0]._id;
+//           const dbUser = data.data[0];
+//           console.log("db user", dbUser);
+//           await setUserSession({
+//             userId: dbUser._id,
+//             token: dbUser.token,
+//             name: dbUser.name,
+//             email: dbUser.email,
+//           });
+//         }
+//       } catch (err) {
+//         console.error("Error saving user:", err);
+//       }
+
+//       return true;
+//     },
+//     async redirect({ url, baseUrl }) {
+//       return "/";
+//     },
+//   },
+// };
+
+// const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST };
 import { setUserSession } from "@/lib/utils/action";
-import {
-  API_URL,
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-} from "@/lib/utils/constants";
+import { API_URL } from "@/lib/utils/constants";
 
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+console.log("ENV TESTtt:", process.env.AUTH_GOOGLE_ID);
+
 export const authOptions = {
-  secret: "fe51dd4501b392b42c5ec446c9287fac1b3aec6e86e4c4fb429de146b4a18bf8",
+  secret: process.env.AUTH_SECRET,
+
   providers: [
     GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+
   session: { strategy: "jwt" },
   pages: { signIn: "/sign-in" },
   basePath: "/api/auth",
   debug: true,
   trustHost: true,
-  // 👇 Hardcode your live domain so NextAuth knows where to redirect
-  //site: "https://book.softplix.com",
 
   callbacks: {
     async signIn({ user }) {
       try {
-        // Call your API to store/fetch DB user
         const res = await fetch(`${API_URL}/sign-in`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
@@ -41,9 +105,9 @@ export const authOptions = {
 
         const data = await res.json();
         if (data.status === 200 && data.data[0]?._id) {
-          user.user_id = data.data[0]._id;
           const dbUser = data.data[0];
-          console.log("db user", dbUser);
+          user.user_id = dbUser._id;
+
           await setUserSession({
             userId: dbUser._id,
             token: dbUser.token,
@@ -57,7 +121,7 @@ export const authOptions = {
 
       return true;
     },
-    async redirect({ url, baseUrl }) {
+    async redirect() {
       return "/";
     },
   },
